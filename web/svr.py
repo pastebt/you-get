45e9dbd17b4b31e146b9e16c77a8e2122812b19c
pastebt <bt@hotmail.com>
2015-02-07 14:10:26 -0800

    add html_*

--- web/svr.py
+++ web/svr.py
@@ -5,8 +5,21 @@ from bottle import get, post, run, request
 from db import init_db, add_one_url
 
 
-@get('/<:re:.*>')
-def index():
+def html_head():
+    return """
+        <html>
+        <head><title>You-Get</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf8">
+        </head>
+        <body>
+        """
+
+
+def html_foot():
+    return "</body></html>"
+
+
+def html_form():
     return """
         <form action="/" method="post">
             URL: <input name="aviurl" type="text" size=60 /><br>
@@ -16,15 +29,22 @@ def index():
         """
 
 
+@get('/<:re:.*>')
+def index():
+    return html_head() + html_form() + html_foot()
+
+
 @post('/')  # or @route('/login', method='POST')
 def do_post():
     aviurl = request.forms.get('aviurl')
     avitil = request.forms.get('avitil')
+    print(avitil)
     add_one_url(aviurl, avitil)
     #fout = open("url_post.txt", "a")
     #fout.write(aviurl + "\n")
     #fout.close()
-    return index() + ("<br>got<p>%s<br>%s</p>" % (avitil, aviurl))
+    return html_head() + index() + (
+           "<br>got<p>%s<br>%s</p>" % (avitil, aviurl)) + html_foot()
 
 
 init_db()
